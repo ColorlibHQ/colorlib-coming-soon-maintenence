9630350c57a9dafbbdfbd608fa7670a887172139
razvanaldea89 <razvan.aldea89@yahoo.ro>
2018-11-02 19:34:58 +0200

    added jquery script

--- colorlib-coming-soon-and-maintenance-mode.php
+++ colorlib-coming-soon-and-maintenance-mode.php
@@ -95,7 +95,8 @@ function colorlibStyleEnqueue( $styles ) {
 
 // Function to enqueue template scripts
 function colorlibScriptEnqueue( $scripts ) {
-	//wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.3.1.min.js' );
+	
+	wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.3.1.min.js' );
 	if ( is_array( $scripts ) ) {
 		foreach ( $scripts as $script ) {
 			if ( $script['location'] != null ) {
