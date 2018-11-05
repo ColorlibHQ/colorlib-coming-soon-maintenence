049556f6da9eba36ea9f06547b35218e4919aa9c
razvanaldea89 <razvan.aldea89@yahoo.ro>
2018-11-05 17:14:28 +0200

    script bug fix. Preview was not working

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1,37 +1,39 @@
 jQuery(window).load(function () {
-    jQuery('textarea.js-ccs-editor').each(function () {
-        var textareaId = jQuery(this).attr('id');
-        var textareaEditor = jQuery(this);
-        wp.editor.initialize(textareaId, {
-            tinymce: {
-                wpautop: true,
-                browser_spellcheck: true,
-                mediaButtons: false,
-                wp_autoresize_on: true,
-                toolbar1: 'bold,italic,link,strikethrough',
-                setup: function (editor) {
-                    editor.on('change', function () {
-                        editor.save();
-                        jQuery(textareaEditor).trigger('change');
-                    });
+    if ('undefined' !== typeof(wp) && 'undefined' !== typeof(wp.customize)) {
+        jQuery('textarea.js-ccs-editor').each(function () {
+            var textareaId = jQuery(this).attr('id');
+            var textareaEditor = jQuery(this);
+            wp.editor.initialize(textareaId, {
+                tinymce: {
+                    wpautop: true,
+                    browser_spellcheck: true,
+                    mediaButtons: false,
+                    wp_autoresize_on: true,
+                    toolbar1: 'bold,italic,link,strikethrough',
+                    setup: function (editor) {
+                        editor.on('change', function () {
+                            editor.save();
+                            jQuery(textareaEditor).trigger('change');
+                        });
+                    }
+                },
+                quicktags: true
+            });
+        });
+
+        wp.customize.panel('colorlib_coming_soon_general_panel', function (section) {
+            section.expanded.bind(function (isExpanding) {
+                var loginURL = CCSurls.siteurl + '?colorlib-coming-soon-customization=true';
+
+                // Value of isExpanding will = true if you're entering the section, false if you're leaving it.
+                if (isExpanding) {
+                    wp.customize.previewer.previewUrl.set(loginURL);
+                } else {
+                    wp.customize.previewer.previewUrl.set(CCSurls.siteurl);
                 }
-            },
-            quicktags: true
+            });
         });
-    });
-
-    wp.customize.panel( 'colorlib_coming_soon_general_panel', function( section ) {
-        section.expanded.bind( function( isExpanding ) {
-            var loginURL = CCSurls.siteurl + '?colorlib-coming-soon-customization=true';
-
-            // Value of isExpanding will = true if you're entering the section, false if you're leaving it.
-            if ( isExpanding ) {
-                wp.customize.previewer.previewUrl.set( loginURL );
-            } else {
-                wp.customize.previewer.previewUrl.set( CCSurls.siteurl );
-            }
-        });
-    });
+    }
 });
 
 
